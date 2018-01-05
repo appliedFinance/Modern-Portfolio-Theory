4b5c3eb57f5ba0c06da99617ca853e628a5cfe77
appliedFinance <atlusr@yahoo.com>
2018-01-04 20:08:13 -0500

    Multiple API calls each with different ticker and returned data identified with each ticker.

--- mpt.js
+++ mpt.js
@@ -4,36 +4,54 @@
 
 function say(s) { console.log(s); }
 
-let IEXTRADING_URL = "https://api.iextrading.com/1.0";
+// IEXTRADING_URL = "https://api.iextrading.com/1.0";
 
-function getDataFromAPI( tkr, callback ) {
+
+function translate(data, tkr, mo) {
+	say(tkr);
+
+}
+
+function getDataFromAPI( tkr, mainObject ) {
 	// build the parameters object
-	let s = `/stock/${tkr}/chart/1y`; 
-	IEXTRADING_URL += s;
-	say("|||  " + s + "   |||");
+	let API_URL = `https://api.iextrading.com/1.0/stock/${tkr}/chart/1y`; 
 	const settings = {
-		'url': IEXTRADING_URL,
+		'url': API_URL,
 		'data': "",
 		'dataType': 'json',
-		'type': 'GET',
-		'success': callback
+		'type': 'GET'
 	};
-	say("*****\n" + settings + "*****\n\n");
-	$.ajax(settings);
-	//$.getJSON( URL, query, callback );
-}
+	say("***** " + `${tkr}` +"\n" + JSON.stringify(settings) + "\n*****");
+	let query =	$.ajax(settings);
 
-function displayResults(data) {
-	say(data);
-}
+	query.done( function(data) {translate(data, tkr, mainObject );} );
+	query.fail( function(tkr) {
+		say("XXX " + tkr + " XXX");
+	});
+
+}//getDataFromAPI
+
+
+
+
+
+
+
+
 
 
 function watcher() {
+	const port = {};
+
 	$('.js-search-form').on("submit", event => {
 		event.preventDefault();
 		let tkr = $(event.currentTarget).find('.js-query').val();
 		// API Call
-		getDataFromAPI(tkr, displayResults);
+		let tkrlist = ["GE","C","MSFT","GOOG","AAPL"];
+		for (let i=0; i<tkrlist.length; i++) {
+			//getDataFromAPI(tkr, displayResults);
+			getDataFromAPI(tkrlist[i], port);
+		}
 	});
 }
 
