54ba1333746ae737dc524b58507fde9b1af0613a
appliedFinance <atlusr@yahoo.com>
2018-01-07 21:54:44 -0500

    Updated intro text, and text in search-form.

--- directions.js
+++ directions.js
@@ -1,9 +1,10 @@
 const APP_DIRECTIONS = `
 	<div class="app-directions" aria-live="assertive">
 		<p class="app-title">Tangent Portfolio Finder</p>
-		<p class="app-text">Enter ticker symbols into the text box.  Use either spaces or commas to separate them.  When you are ready click the Fetch Button to begin.</p>
-		<p><a id="app-anchor" href="#"></a></p>
-		</div>
+		<p class="app-text">Modern Portfolio Theory assumes investors are risk adverse, meaning given two portfolios offering equivalent expected return an investor will prefer to invest money in the less risky one.</p>
+		<p class="app-text">Diversification happens when risky stocks are combined together into a portfolio and the overall combined risk is lowered due to offsetting movements in stock prices.  Some stocks go up while others go down.</p>
+		<p class="app-text">The tangency portfolio is a unique collection of stock weights.  It is desirable to know where the tangency portfolio lies in a risk versus return trade-off because this portfolio has very awesome qualities.</p>
+	</div>
 		<p class="app-text">Click to Start (tab+enter)</p>
 	</div>
 	`;
